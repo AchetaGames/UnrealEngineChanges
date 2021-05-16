7eae2b60c3c3673f1945ceae3878e8212cd2d59b
Milan Šťastný <milan@acheta.games>
2021-05-16 18:44:53 +0200

    Update manage_issues.py

--- manage_issues.py
+++ manage_issues.py
@@ -12,18 +12,25 @@ open_issues = issues_repo.get_issues(state='open')
 for issue in open_issues:
     print(issue.title)
     pr = re.search("(?P<url>https?://[^\s]+)", issue.body).group("url")
-    print("Link: {}".format(pr))
     r = re.search("/(?P<repo>[0-9a-zA-z]+/[0-9a-zA-z]+)/pull/(?P<id>[0-9]+)", pr)
     if r:
         pr_repo = g.get_repo(r.group("repo"))
-        print("repo: {}".format(r.group("repo")))
-        print("pr_id: {}".format(r.group("id")))
         request = pr_repo.get_pull(int(r.group("id")))
+        print("\tHas linked PR: {}/pull/{}".format(pr_repo, r.group("id")))
         for label in request.labels:
-            if label.name=="Accepted":
-                issue.add_to_labels(issues_repo.get_label("Accepted"))         
+            if label.name == "Accepted":
+                print("\tThe PR is Accepted")
+                issue.add_to_labels(issues_repo.get_label("Accepted"))
         if request.merged or request.closed_at:
+            print("\tThe PR is Accepted")
             issue.edit(state="Closed")
-            continue      
+            continue
         if not request.mergeable:
+            print("\tPR is blocked")
             issue.add_to_labels(issues_repo.get_label("Merge Conflict"))
+        else:
+            print("\tPR is Waiting for Review")
+            issue.add_to_labels(issues_repo.get_label("Waiting for Approval"))
+    else:
+        print("\tNo PR linked but there is a patch link")
+        issue.add_to_labels(issues_repo.get_label("Patch"))
